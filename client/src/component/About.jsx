cb1df143a2fab85bdc19b1ad4987943788b36271
18Palku <bhartipalak18@gmail.com>
2023-08-28 16:42:09 +0530

    About

--- client/src/component/About.jsx
+++ client/src/component/About.jsx
@@ -1,8 +1,35 @@
+import "../Styles/About.css";
+import img from "../assets/back3.jpg";
+
 const About = () => {
   return (
-    <div>
-      <h1>About wbkjwenlwjwk</h1>
-    </div>
+    <div class="main_content2" id="content2">
+<div class="ninenty">
+<div class="image1">
+<img class="image3" src={img}/>
+
+</div>
+<div class="text1">
+<p class="kj"> About Our Store</p>
+<h1 class="le">Here Comes An <br/>Intro About <br/>Store</h1>
+<p class="op">We’ve put our proceeded progress down<br/> to knowing what buyers and organizations<br/> require,
+ and having a broad scope of the best<br/> frill for the most recent gadgets — both<br/> integrated with our reality class client benefit.</p>
+
+
+<i class="fa fa-headphones" aria-hidden="true"></i>
+<p class="ha">Get Support</p>
+<hr/>
+<i class="fa fa-check" aria-hidden="true"></i>
+<p class="zz">As an organization, we are committed to consistent advancement </p>
+<i class="fa fa-check" aria-hidden="true"></i>
+<p class="zc">Our committed and proactive group of item specialists guarantee that we’re reliably 
+capable offer the most recent and best embellishments .</p>
+
+
+
+</div>
+</div>
+</div>
   );
 };
 export default About;
