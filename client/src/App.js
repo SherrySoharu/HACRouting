7e9076c15b3601f017d512988a6df5a54513e4ff
18Palku <bhartipalak18@gmail.com>
2023-08-31 14:36:46 +0530

    about page

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,20 @@ import About from "./component/About";
 import Contact from "./component/Contact";
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./component/NavBar";
+import Footer from "./component/Footer";
 
 function App() {
   return (
     <div>
       <NavBar />
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="/Contact-us" element={<Contact />} />
       </Routes>
+      <Footer/>
+      
     </div>
   );
 }
--- client/src/component/About.jsx
+++ client/src/component/About.jsx
@@ -10,23 +10,24 @@ const About = () => {
 
 </div>
 <div class="text1">
-<p class="kj"> About Our Store</p>
-<h1 class="le">Here Comes An <br/>Intro About <br/>Store</h1>
-<p class="op">We’ve put our proceeded progress down<br/> to knowing what buyers and organizations<br/> require,
- and having a broad scope of the best<br/> frill for the most recent gadgets — both<br/> integrated with our reality class client benefit.</p>
+<p class="kj"> Welcome to Cosmic Discovery</p>
+<h1 class="le">Unveiling the Mysteries <br/>of the Universe </h1>
+<p class="op">Embark on a journey through the cosmos and uncover the secrets of space.
+        From breathtaking galaxies to mesmerizing nebulae, we invite you to
+        explore the wonders that the universe has to offer. Our mission is to
+        bring the beauty and excitement of space right to your screen.</p>
 
 
 <i class="fa fa-headphones" aria-hidden="true"></i>
 <p class="ha">Get Support</p>
-<hr/>
+{/* <hr/> */}
 <i class="fa fa-check" aria-hidden="true"></i>
 <p class="zz">As an organization, we are committed to consistent advancement </p>
 <i class="fa fa-check" aria-hidden="true"></i>
-<p class="zc">Our committed and proactive group of item specialists guarantee that we’re reliably 
-capable offer the most recent and best embellishments .</p>
-
-
-
+<p class="zc">Our website is your gateway to the universe. Discover the latest space
+        news, view stunning images captured by telescopes and spacecraft, and
+        learn about groundbreaking space missions that push the boundaries of
+        human exploration. Join us on this cosmic journey!.</p>
 </div>
 </div>
 </div>
